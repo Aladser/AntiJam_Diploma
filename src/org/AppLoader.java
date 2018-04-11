49d00e6ffb9ae4fff3ca4e1ef51a2ca11f8c9828
Aladser <aladser@gmail.com>
2018-04-11 13:16:48 +0900

    1129 RSCodec

--- src/org/AppLoader.java
+++ src/org/AppLoader.java
@@ -19,25 +19,10 @@ public class AppLoader {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(org.views.MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-
-        RSCodec rsc = new RSCodec(4, 2, 3);
-        int[] src = {0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,1};
-        BitSet binSrc = new BitSet();
-        binSrc.set(src.length);
-        for(int i=0; i<src.length; i++) if(src[i]==1) binSrc.set(i);
-        BitSet code = rsc.encode(binSrc);        
-        BitSet res = rsc.decode(code);
-        for(int i=0; i<res.length(); i++){
-            if(res.get(i) != binSrc.get(i)){
-                System.out.println("Ошибка передачи");
-                break;
-            }
-            if(i == res.length()-1) System.out.println("Отправленные и полученные данные равны");
-        }
         
         /* Вызов главного окна */
-        //pf = new org.views.ProgressFrame();
-        //new org.views.MainFrame().setVisible(true);
+        pf = new org.views.ProgressFrame();
+        new org.views.MainFrame().setVisible(true);
     }
     
     
