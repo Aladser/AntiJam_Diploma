4fe4ef426e385032d46f2d4eb667ba20e7997309
Aladser <aladser@gmail.com>
2018-04-22 18:10:22 +0900

    1201  ConvolCodec

--- src/org/AppLoader.java
+++ src/org/AppLoader.java
@@ -1,6 +1,7 @@
 package org;
 
 import java.util.BitSet;
+import org.models.BinOperations;
 import org.models.codecs.ConvolCodec;
 
 
@@ -19,11 +20,32 @@ public class AppLoader {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(org.views.MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        
-        ConvolCodec c = new ConvolCodec(12, 9, 3);
-        c.createSyndrTable();
-        
+        /*
+        ConvolCodec codec = new ConvolCodec(12, 9, 3);
+        int[] arr = {1,1,1, 0,0,0, 1,1,1, 0,0,0, 1,1,1, 0,0,0, 1,1,1, 0,0,0};
+        BitSet Ax = new BitSet();
+        Ax.set(arr.length);
+        for(int i=0; i<Ax.length()-1; i++) if(arr[i]==1) Ax.set(i);
+        BitSet Cx = codec.encode(Ax);
+        System.out.println("---- ---- x---");
+        Cx.clear(8);
+        codec.decode(Cx);
+        Cx.set(8);
+        System.out.println("---- ---- -x--");
+        Cx.clear(9);
+        codec.decode(Cx);
+        Cx.set(9);
+        System.out.println("---- ---- --x-");
+        Cx.clear(10);
+        codec.decode(Cx);
+        Cx.set(10);
+        System.out.println("---- ---- ---x");
+        Cx.clear(11);
+        codec.decode(Cx);
+        Cx.set(11);
+        */
         /* Вызов главного окна */
-        //new org.views.MainFrame().setVisible(true);
-    } 
+        new org.views.MainFrame().setVisible(true);
+    }
+    
 }
--- src/org/models/codecs/ConvolCodec.java
+++ src/org/models/codecs/ConvolCodec.java
@@ -20,12 +20,19 @@ public class ConvolCodec extends Codec{
      */
     public final int NUM_BITS;
     private final int num_blocks;   // число блоков
+    public final int[] syndroms;   // синдромы
     
+    /**
+     * @param n длина кодового слова
+     * @param k длина инфослова
+     * @param numbits число бит инфоблока
+     */
     public ConvolCodec(int n, int k, int numbits){
         K = k;
         N = n;
         NUM_BITS = numbits;
         num_blocks = k / NUM_BITS;
+        syndroms = createSyndrTable();
     }
     
     @Override
@@ -65,75 +72,92 @@ public class ConvolCodec extends Codec{
 
     @Override
     public BitSet decode(BitSet Cx) {
-        int S = 0;
-        int si = 0;
-        System.out.print(BinOperations.showBitSet(Cx, 4));
-        
+        //System.out.println("С(x)  "+BinOperations.showBitSet(Cx, 4));
         BitSet Ax = new BitSet();
         Ax.set((Cx.length()-1)*K/N);
-        int ci=0, ai=0;
-        // Декодирование
-        int checkBit = 0;
-        for(; ci<Cx.length()-1; ci++, ai++){
-            if(Cx.get(ci)){
-                Ax.set(ai);
-                checkBit^=1;
+        int S=0, si=0;    // синдром
+        int checkBit;
+        for(int ai=0, ci=0; ci<Cx.length()-1; ai+=3, ci+=4){
+            if(Cx.get(ci)) Ax.set(ai);
+            if(Cx.get(ci+1)) Ax.set(ai+1);
+            if(Cx.get(ci+2)) Ax.set(ai+2);
+            checkBit = (Cx.get(ci)?1:0) ^ (Cx.get(ci+1)?1:0) ^ (Cx.get(ci+2)?1:0); // XOR инфобит
+            if(ci >= 4) checkBit ^= (Cx.get(ci-4)?1:0) ^ (Cx.get(ci-3)?1:0);       // XOR бит предыдущих кадров
+            if(ci > 4) checkBit ^= (Cx.get(ci-8)?1:0) ^ (Cx.get(ci-6)?1:0);        // XOR бит предыдущих кадров
+            S += checkBit ^ (Cx.get(ci+3)?1:0);                                    // проверка на ошибку
+            // если собран синдром
+            if(si == 2){
+                //if(S != 0) System.out.println("S = "+Integer.toBinaryString(S)+"("+S+")");
+                if(S==0) si=0;
+                // Если синдром имеет 1 в старшем разряде
+                if(S >= 4){
+                    int ind = -1;
+                    for(int i=0; i<syndroms.length; i++) if(syndroms[i]==S) ind=i;
+                    if(Cx.get(ci+ind-8)) Cx.clear(ci+ind-8);
+                    else Cx.set(ci+ind-8);
+                    if(Ax.get(ai+ind-6)) Ax.clear(ai+ind-6);
+                    else Ax.set(ai+ind-6);
+                    S=0;
+                }
+                // если синдром неполный, продолжаем его набор
+                else S<<=1;
             }
-            if((ai+1)%3==0){
-                ci++;
-                if(ai+1 >= 6){
-                    if(Cx.get(ci-7)) checkBit^=1;
-                    if(Cx.get(ci-6)) checkBit^=1;
-                }
-                if(ai+1 > 6){
-                    if(Cx.get(ci-11)) checkBit^=1;
-                    if(Cx.get(ci-9)) checkBit^=1;
-                    
-                }
-                checkBit ^= Cx.get(ci)?1:0;
-                
-                S += checkBit;
-                si++;
-                if(si == num_blocks){
-                    if(S < 2){
-                        System.out.println(" | 00"+Integer.toBinaryString(S)+" | "+S);
-                    }
-                    else if(S < 4)
-                        System.out.println(" | 0"+Integer.toBinaryString(S)+" | "+S);
-                    else
-                        System.out.println(" | "+Integer.toBinaryString(S)+" | "+S);
-                    
-                }
+            // продолжение набора синдрома
+            else{
                 S<<=1;
-                
-                checkBit=0;
+                si++;
             }
         }
-        
-        //System.out.println("Ax  "+BinOperations.showBitSet(Ax, 3));
-        int ind = Ax.length()-1-((Ax.length()-1)%8);
-        for(int i=ind+1; i<Ax.length(); i++) Ax.clear(i);
-        
-        //System.out.println(BinOperations.showBitSet(Ax, 3));
+        //System.out.println("С(x)  "+BinOperations.showBitSet(Cx, 4));
+        //System.out.println("A(x)  "+BinOperations.showBitSet(Ax, 3));
         return Ax;
     }
+
+
     
-    public void createSyndrTable(){
-        int[] syndroms = new int[N];
-        BitSet Ax = new BitSet(); 
-        int[] arr = {0,0,0, 0,0,0, 0,0,0};
-        Ax.set(arr.length);
-        for(int i=0; i<Ax.length()-1; i++) if(arr[i]==1) Ax.set(i);
-        BitSet Cx = encode(Ax);
-        
-        System.out.println("Ex             | S");
-        for(int i=0; i<12; i++){
-            Cx.set(i);
-            decode(Cx);
-            Cx.clear(i);
+    /**
+     * Создает массив синдромов
+     * @return 
+     */
+    private int[] createSyndrTable(){
+        int[] res = new int[NUM_BITS * N/K];
+        // E(x)
+        BitSet Ax = new BitSet(); Ax.set(K);
+        BitSet Ex = encode(Ax);
+        // формирование синдромов для E(x)
+        int S, si=0, checkBit=0; // синдром, i по S, бит четности
+        // проход по E(x) с переносом вправо ошибочного бита при итерации
+        for(int i=0; i<NUM_BITS * N/K; i++){
+            Ex.set(i); // создает ошибочный бит     
+            // декодирование
+            S = 0; 
+            for(int ai=0, ci=0; ci<Ex.length()-1; ci++, ai++){
+                if(Ex.get(ci)) checkBit^=1; // XOR инфобит
+                // XOR предыдущих бит
+                if((ai+1)%NUM_BITS==0){
+                    ci++;
+                    if(ai+1 >= NUM_BITS*2){
+                        if(Ex.get(ci-7)) checkBit^=1;
+                        if(Ex.get(ci-6)) checkBit^=1;
+                    }
+                    if(ai+1 > NUM_BITS*2){
+                        if(Ex.get(ci-11)) checkBit^=1;
+                        if(Ex.get(ci-9)) checkBit^=1;
+                    }
+                    checkBit ^= Ex.get(ci)?1:0;
+                    S += checkBit;
+                    si++;
+                    if(si == num_blocks){
+                        res[i] = S;
+                        si=0;
+                    }
+                    S<<=1;
+                    checkBit=0;
+                }
+            }            
+            Ex.clear(i); // исправляет ошибочный бит
         }
-        
-        
+        return res;
     }
-   
+    
 }
